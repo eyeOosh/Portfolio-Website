64af5c3ea224a9fa60b995ecacbf999c8d02805f
eyeOosh <109489470+eyeOosh@users.noreply.github.com>
2026-01-29 21:12:34 -0800

    All files so far

---
+++ script.js
@@ -0,0 +1,29 @@
+function openProject(button) {
+    console.log("Button clicked:", button.id);
+    url = "";
+    switch (button.id) {
+        case "project1Button":
+            url = "https://www.github.com/eyeOosh/116A-Project";
+            break;
+        case "project2Button":
+            url = "https://www.github.com/eyeOosh/British-Airways-Prediction-Model";
+            break;          
+        case "project3Button":
+            url = "https://www.github.com/eyeOosh/Tata-Data-Visualization";
+            break;
+        case "project4Button":
+            url = "https://www.github.com/eyeOosh/AI-Yahtzee-Bot";
+            break;
+        case "project5Button":
+            url = "https://www.github.com/eyeOosh/BCG-Gen-AI";
+            break;
+        case "project6Button":
+            url = "https://www.github.com/eyeOosh/Recipe-App";
+            break;       
+        default:
+            url = "https://www.github.com/eyeOosh"; 
+            break;   
+    }
+
+    window.open(url, '_blank').focus();
+}
